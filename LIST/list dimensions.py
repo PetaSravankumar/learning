ec88c1170edbf0fc9888929a82d9fe7591a74256
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-09-15 13:02:25 +0530

    add all the code

--- LIST/list dimensions.py
+++ LIST/list dimensions.py
@@ -4,3 +4,12 @@ l1=[10,20,['sravan','kumar'],4]
 print(l1)
 print(len(l1))
 print(type(l1))
+
+
+# this is the way of accessing the 2D list list diclar the original list and and close the bracket and start the nested lsit
+print(l1[2][1])
+
+l3=[10,20,["sravan",'kumar',['sitha','radha','lakshmi'],'sam'],55]
+print(len(l3))
+print(l3)
+print(l3[2][2][0])
