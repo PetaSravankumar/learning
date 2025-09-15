0b03f76e62359cddd9b366422f47c3098522b349
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-09-15 12:33:07 +0530

    add all the code

--- LIST/example list.py
+++ LIST/example list.py
@@ -5,3 +5,17 @@ print(type(l))
 l1=[10,'sravan',4.3]
 print(l1)
 print(type(l1))
+
+l2=[]
+i=0
+while True:
+    num=int(input("enter the number :"))
+    l2.insert(i,num)
+    i=i+1
+    print("do you want to continue")
+    choose=int(input("enter the choice : 1:yes , 2: No "))
+    if choose==1:
+        continue
+    else:
+        break
+print(l2)
