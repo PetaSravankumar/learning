7e3a651c8be399ab1c7e8c0efea2ee734ff42030
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-09-20 13:16:53 +0530

    add all the code

--- LIST/zip function.py
+++ LIST/zip function.py
@@ -2,4 +2,4 @@ name=["virat","dhoni","ABD","maxwell"]
 f_nam=[18,7,17,32]
 country=["ind","indi","So Af","Astr"]
 ipl=['rcb','csk','rcb','pb']
-res=list(zip(name,))
+res=list(zip(name,f_nam,country,ipl))
