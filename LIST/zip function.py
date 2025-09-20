49c8ea880d10b522d731a129f18231c60f33d44e
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-09-20 13:21:11 +0530

    add all the code

--- LIST/zip function.py
+++ LIST/zip function.py
@@ -1,6 +1,10 @@
+from itertools import zip_longest
 name=["virat","dhoni","ABD","maxwell"]
 f_nam=[18,7,17,32]
 country=["ind","indi"]
 ipl=['rcb','csk','rcb','pb']
 res=list(zip(name,f_nam,country,ipl))
 print(res)
+
+res1=list(zip_longest(name,f_nam,country,ipl))
+print(res1)
