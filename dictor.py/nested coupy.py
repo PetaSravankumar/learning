a0b947b978323b596e14e4e6dc8feb1445e65d73
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-09-26 13:05:45 +0530

    add all the code

--- dictor.py/nested coupy.py
+++ dictor.py/nested coupy.py
@@ -3,8 +3,10 @@ circket={"name":"virat",'century':82,"egfgs":{"num1":"genelia","num2":"ritika"}}
 print(circket)
 c1=circket.copy()
 print(c1)
-c2=circket.copy()
-print(c2)
+c3=circket.copy()
+c2=copy.deepcopy(circket)
+print(c3)
 circket["egfgs"]['num3']="tamanla"
-print(c2)
+print(c3)
 print(c1)
+print(c2)
