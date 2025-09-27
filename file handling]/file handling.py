dec57ec7bf31e2a5c4ac6212f5cbf0dc235cf1b3
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-09-27 12:54:54 +0530

    add all the code

--- file handling]/file handling.py
+++ file handling]/file handling.py
@@ -1,4 +1,9 @@
-name=input("enter the name")
+# name=input("enter the name")
+# prt=open("sravan.txt",'w')
+# prt.write(name)
+# prt.close()
+
+name=input("enter the name :")
 prt=open("sravan.txt",'a')
 prt.write(name)
 prt.close()
