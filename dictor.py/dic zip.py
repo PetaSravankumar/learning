2e1c82b618dc8946a4319206a75b3f5cc5dcf32a
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-09-27 12:45:01 +0530

    add all the code

---
+++ dictor.py/dic zip.py
@@ -0,0 +1,8 @@
+id=[101,102,103,104]
+name=['loke','shiva','sravan','kalyan']
+res=dict(zip(id,name))
+print(res)
+mob=[123,4532,4523,2343]
+addr=['bidra','russia','india','majestic']
+res1=dict(zip(id,name,mob,addr))
+print(res1)
---
+++ file handling]/file handling.py
@@ -0,0 +1,4 @@
+name=input("enter the name")
+prt=open("sravan.txt",'w')
+prt.write(name)
+prt.close()
