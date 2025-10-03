9795a2452c9083019f6fd4687b454d8dd43498a5
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-10-03 13:05:34 +0530

    add all the code

---
+++ multi threading/single threading.py
@@ -0,0 +1,9 @@
+import time
+class Demo:
+    def printName(self):
+        l=["rathu","sravan",'suni']
+        for i in l:
+            print(i)
+            time.sleep(3)
+    def printNumebers(self):
+        
