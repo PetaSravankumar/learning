41b4b7754ddd8549fd6d8bee872ba3c98c50c113
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-10-06 09:33:50 +0530

    add all the code

--- multi threading/even odd.py
+++ multi threading/even odd.py
@@ -5,15 +5,19 @@ class even(Thread):
         for i in range(100):
             if i%2==0:
                 print(i)
-                time.sleep(2)
+                # time.sleep(2)
 class  odd(Thread):
     def run(self):
-        for i in range(10):
+        for i in range(100):
             if i%2!=0:
                 print(i)
-                time.sleep(2)
+                # time.sleep(2)
 t1=even()
 t2=odd()
 t1.start()
-time.sleep(0.5)
+# t1.join()
 t2.start()
+# t2.join()
+# t1.start()
+# time.sleep(0.5)
+# t2.start()
