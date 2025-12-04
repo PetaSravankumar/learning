589a7684c4993c868d5d07bc5978c7937aa2e79e
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2025-12-04 11:08:45 +0530

    Added local project files

--- numpy and pandas/2dlist.py
+++ numpy and pandas/2dlist.py
@@ -1,2 +1,18 @@
-import numpy as np
-print(np.__version__)
+l=[]
+a=[]
+for i in range(3):
+    for j in range(3):
+        k=int(input("enter the number :"))
+        a.append(k)
+    l.append(a)
+    a=[]
+print(l)
+sum1=0
+sum2=0
+for i in  range(len(l)):
+    for j in range(len(l[0])):
+        if i==j:
+            sum1+=l[i][j]
+        elif i+j==len(l)-1:
+            sum2+=l[i][j]
+    print()
